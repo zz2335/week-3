ab537f16cb61172904b9aea3790e6613e58e7a9c
Danil <kidvsking@yahoo.com>
2015-09-06 15:31:11 -0400

    update

--- app_finished.py
+++ app_finished.py
@@ -7,9 +7,5 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/getData/")
-def getData():
-    return "this is data"
-
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
