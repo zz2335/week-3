f0481e08166b42f70ca91e302b7024e22d8681cf
Danil <kidvsking@yahoo.com>
2015-09-06 15:28:31 -0400

    update

--- app_finished.py
+++ app_finished.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from flask import render_template
 
+import json
+import time
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +12,9 @@ def index():
 
 @app.route("/getData/")
 def getData():
-    return "this is data"
+	print "getting the data"
+	output = {"out":time.strftime('%X %x %Z')}
+	return json.dumps(output)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=5000,debug=True,threaded=True)
